0e5ff42192c0beda95914a485d6a42a2c16fac11
Vinay Suryadevara <frags.vinay@gmail.com>
2017-07-18 12:35:18 -0700

    Added code for very simple Spanish translate bot

---
+++ app.js
@@ -0,0 +1,27 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+const translate = require('google-translate-api');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    translate(session.message.text, {to: 'es'}).then(res => {
+        session.send(res.text);
+    }).catch(err => {
+        console.error("Error" - err);
+    }); 
+});
